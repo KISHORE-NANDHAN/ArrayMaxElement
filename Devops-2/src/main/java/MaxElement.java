fc96e160dffec30352c868aaf375bc2b34043403
KISHORE NANDHAN <nandhakishorejajula@gmail.com>
2025-09-16 13:44:20 +0530

    Added code

--- Devops-2/src/main/java/MaxElement.java
+++ Devops-2/src/main/java/MaxElement.java
@@ -2,8 +2,11 @@
 public class MaxElement {
 	public static int maxElement(int arr[]) {
 		int max = Integer.MIN_VALUE;
-		
-		
+		for(int num : arr) {
+			if(num > max) {
+				max = num;
+			}
+		}
 		return max;
 		
 	}
